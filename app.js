7bd9622e8088b7f14efdede46dfd93e1bbd88dbe
Guilherme <guilhermehf23@gmail.com>
2023-04-20 21:55:44 -0300

    Segundo Commit

--- app.js
+++ app.js
@@ -4,8 +4,27 @@ const app = express();
 const PORTA = 3000;
 
 
-app.get("/hello", (req, res) => {
-    res.send("Hello World");
+app.get("/api/clientes", (req, res) => {
+    res.send("Lista de Clientes");
+})
+
+app.get("/api/clientes/:id",(req, res) => {
+    const id = req.params.id;
+    res.send(`Buscar por id: ${id}`);
+})
+
+app.post("/api/clientes", (req, res) => {
+    res.send("Cadastro de Clientes");
+})
+
+app.put("api/clientes/:id", (req, res) => {
+    const id = req.params.id;
+    res.send(`Atualizando o Cliente ${id}`);
+})
+
+app.delete("api/clientes/:id", (req, res) => {
+    const id = req.params.id;
+    res.send(`Deletando o Cliente ${id}`);
 })
 
 app.listen(PORTA, () => {
