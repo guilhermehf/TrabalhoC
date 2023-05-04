2e4815971f80c31fc933446846aa340f3fe1da91
Guilherme <guilhermehf23@gmail.com>
2023-05-04 19:01:59 -0300

    Segundo CRUD Segundo Commit

--- app_login.js
+++ app_login.js
@@ -3,8 +3,28 @@ const express = require('express')
 const app = express();
 const PORTA = 3000;
 
-app.get("/hello", (req, res) => {
-    res.send("Hello World");
+app.get("/api/login", (req, res) => {
+    res.send("Lista de Login");
+})
+
+app.get("/api/login/:id", (req, res) => {
+    const id = req.params.id;
+    res.send(`Buscar pelo id: ${id}`);
+})
+
+app.post("/api/login", (req, res) => {
+    res.send("Cadastro de Login");
+})
+
+app.put("/api/login/:id", (req, res) => {
+    const id = req.params.id;
+    res.send(`Atualizando o login ${id}`);
+})
+
+app.delete("/api/login/:id", (req, res) => {
+    const id = req.params.id;
+    res.send(`Deletando o Login ${id}`);
+
 })
 
 app.listen(PORTA, () => {
