53dfe665df5d38feb75e09f42338f405ad74822f
Guilherme <guilhermehf23@gmail.com>
2023-05-04 20:13:36 -0300

    Segundo CRUD Terceiro Commit

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
+
 const express = require('express')
 const  rotaClientes = require('./rotas/clientes_rotas')
 
 const app = express();
 const PORTA = 3000;
+const mongoose = require('mongoose')
 
 
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-app.use('/api/clientes', rotaClientes);
+mongoose.connect('mongodb://127.0.0.1:27017/cadastro_clientes', {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+    
+  }).then(()=> {
+    console.log('BD conectado');
+  })
+  .catch((error)=> {
+    console.log('Error ao conectar ao BD', error);
+  });
+mongoose.Promise = global.Promise
 
+app.use('/api/clientes', rotaClientes);
 
 app.listen(PORTA, () => {
     console.log(`Servidor iniciado na porta ${PORTA}`);
--- app_login.js
+++ app_login.js
@@ -3,27 +3,100 @@ const express = require('express')
 const app = express();
 const PORTA = 3000;
 
+let listaLogin = [
+    {id: 1, login: "admin", senha: "admin"},
+    {id: 2, login: "predio", senha: "predio"},
+    {id: 3, login: "aluno", senha: "123"},
+]
+
+let idAutoIncrement = 4;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 app.get("/api/login", (req, res) => {
-    res.send("Lista de Login");
+    res.json(listaLogin);
 })
 
 app.get("/api/login/:id", (req, res) => {
     const id = req.params.id;
-    res.send(`Buscar pelo id: ${id}`);
+
+    const loginEncontrado = listaLogin.find((login) => {
+        return (login.id == id);
+    })
+
+    if(loginEncontrado){
+        return res.json(loginEncontrado);
+    }
+    else {
+        return res.status(404).json({ Erro: "Login nao encontrado"});
+    }
 })
 
 app.post("/api/login", (req, res) => {
-    res.send("Cadastro de Login");
+    
+
+    //receber o login
+    const novoLogin = req.body;
+    //validar os dados
+
+    if(novoLogin && novoLogin.login && novoLogin.senha){
+        //se OK, cadastro os logins e retorna 201
+        novoLogin.id = idAutoIncrement++;
+        listaLogin.push(novoLogin);
+        return res.status(201).json(novoLogin);
+    }
+    else{
+        //senao retorna 400
+        return res.status(400).json({
+            Erro: "Nome e/ ou senha sao obrigatorios"
+
+        });
+    }
 })
 
 app.put("/api/login/:id", (req, res) => {
     const id = req.params.id;
-    res.send(`Atualizando o login ${id}`);
+    
+
+    const loginAlterar = req.body;
+
+    if(!loginAlterar || !loginAlterar.login || !loginAlterar.senha){
+        return res.status(400).json({
+            Erro: "Nome e/ou senha sao obrigatorios"
+        });
+    }
+
+    const loginEncontrado = listaLogin.find((login) => {
+        return (login.id == id);
+    })
+
+    if(loginEncontrado){
+        loginEncontrado.login = loginAlterar.login;
+        loginEncontrado.senha = loginAlterar.senha;
+    }
+    else {
+        return res.status(404).json({ Erro: "Login nao encontrado"});
+    }
 })
 
 app.delete("/api/login/:id", (req, res) => {
     const id = req.params.id;
     res.send(`Deletando o Login ${id}`);
+
+    const indiceLogin = listaLogin.findIndex(
+        (login) => {
+            return (login.id == id);
+        }
+    )
+
+    if(indiceLogin >= 0){
+        const loginDeletado = listaLogin.splice(indiceLogin, 1)[0];
+        return res.json(loginDeletado);
+    }
+    else {
+        return res.status(404).json({ Erro: "Login não encontrado"});
+    }
 
 })
 
